a814144aa310aa233f076e9b5a7c43dc1019ca1b
marcin <marcin.krze@icloud.com>
2018-07-08 14:49:23 +0100

    adding methods for: transfers, withdraws, balance

--- src/main/java/model/Bank.java
+++ src/main/java/model/Bank.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
  * @author marcin
  */
 public class Bank {
+    
     //stores all of the cusotmers
     private ArrayList<Customer> customers;
     //stores the last account number created so any new account gets an
@@ -70,8 +71,83 @@ public class Bank {
         return false;
     }
     
+    //method used for money transfer operations, returns true if successful
+    //false if not, customer can transfer money only from one of thier own 
+    //acounts
+    public boolean transferMoney(String cusomterLogin, int accountFrom, 
+            int accountTo, double amount){
+        
+        //loops thourgh all cutomers
+        for(Customer c: customers){
+            //temp variables to store refrences to accounts
+            Account accountFromTemp = null;
+            Account accountToTemp = null;
+            
+                //loops thorough all accounts for a customer and 
+                //checks if the number for account from exist and belongs to
+                //that customer
+                for(Account a:c.getAccounts()){
+                    if((a.getAccountNumber()==accountFrom)&&
+                            (c.getLogin().equals(cusomterLogin))){
+                        
+                        accountFromTemp=a;
+                    }
+                    //checks if accountTo number exist
+                    if(a.getAccountNumber()==accountTo){
+                        accountToTemp=a;
+                    }
+                    //if both numbers are found check if acount from has
+                    //sufficeint funds if so trnasfers money otherwise 
+                    //methods returns false
+                    if((accountFromTemp!=null) && (accountToTemp!=null)){
+                        if(accountFromTemp.getBalance() >= amount ){
+                            accountFromTemp.setBalance(accountFromTemp.getBalance()-amount);
+                            accountToTemp.setBalance(accountToTemp.getBalance()+amount);
+                            return true;
+                        }
+                    }
+                }
+        }
+        return false;
+    }
     
+    //method used to withdraw money, returns true if successfull, false if not
+    public boolean withdraw(String login, int accountNumber, double amount){
+        //temp var to store account pointer
+        Account tempAccountReference = null;
+        for(Customer c:customers){
+            if(c.getLogin().equals(login)){
+                for(Account a:c.getAccounts()){
+                    if(a.getAccountNumber()==accountNumber){
+                        tempAccountReference=a;
+                        break;
+                    }
+                }
+            }
+            //if account is found break out of the loop
+            if(tempAccountReference!=null){
+                break;
+            }
+        }
+        //if account has sufficient fundings withdraw
+        if(tempAccountReference.getBalance()>=amount){
+            tempAccountReference.setBalance(tempAccountReference.getBalance()-amount);
+        }
+        return false;
+    }
     
-    
+    //returns balance for the account, reutns -1 if account or login not found
+    public double getBalance(String login, int accountNumber){
+        for(Customer c:customers){
+            if(c.getLogin().equals(login)){
+                for(Account a:c.getAccounts()){
+                    if(a.getAccountNumber()==accountNumber){
+                        return a.getBalance();
+                    }
+                }
+            }
+        }
+        return -1.0;
+    }
     
 }
